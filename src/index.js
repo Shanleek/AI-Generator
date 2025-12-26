1fa5f5fdd92a672112bae60a1dc4b401a2be2968
Shanleek <shannonkraaz@gmail.com>
2025-12-26 09:55:27 +0800

    added for JS added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Welcome to my AI generator");
+function writePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "Blahblah blah",
+    autoStart: true,
+    delay: 1.5,
+    cursor: "",
+  });
+}
+
+let generatePoemFormElement = document.querySelector("#poem-generator-form");
+generatePoemFormElement.addEventListener("submit", writePoem);
